3b62ea01b8f060d37645c6e1c7db48cd65a6dbea
Michael Pappas <mixalis.pappas23@gmail.com>
2021-07-22 20:37:05 +1000

    file: starting on navBar

---
+++ client/src/app/components/navBar/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import styled from 'styled-components';
+import tw from 'twin.macro';
+
+
+const NavbarContainer = styled.div`
+    min-height: 68px;
+    ${tw`
+        w-full
+        max-w-2xl
+        flex-row
+        items-center
+        lg:pl-12
+        lg:pr-12
+        justify-between
+    `}
+
+`;
+
+const LogoContainer = styled.div`
+`;
+
+export function NavBar() {
+    return <NavbarContainer>
+        
+    </NavbarContainer>
+    
+}
