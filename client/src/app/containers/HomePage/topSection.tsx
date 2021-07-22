38f2736da6745f7d381835e30a7b6f19337c1a5c
Michael Pappas <mixalis.pappas23@gmail.com>
2021-07-22 23:22:28 +1000

    fix: fixing some typos

--- client/src/app/containers/HomePage/topSection.tsx
+++ client/src/app/containers/HomePage/topSection.tsx
@@ -4,37 +4,36 @@ import tw from "twin.macro";
 
 
 const TopSectionContainer = styled.div`
-    min-height: 400px;
-    margin-top: 6em;
-    ${tw`
-        w-full
-        max-w-screen-2xl
-        flex
-        justify-between
-        pl-3
-        pr-3
-        lg:pl-12
-        lg:pr-12
-        
-    `};
+  min-height: 400px;
+  margin-top: 6em;
+  ${tw`
+    w-full
+    max-w-screen-2xl
+    flex
+    justify-between
+    pl-3
+    pr-3
+    lg:pl-12
+    lg:pr-12
+  `};
 `;
 
 const LeftContainer = styled.div`
-    ${tw`
-        w-1/2
-        flex
-        flex-col
-    `};
+  ${tw`
+    w-1/2
+    flex
+    flex-col
+  `};
 `;
 
 const RightContainer = styled.div`
-    ${tw`
-        w-1/2
-        flex
-        flex-col
-        relative
-        mt-20
-    `};
+  ${tw`
+    w-1/2
+    flex
+    flex-col
+    relative
+    mt-20
+  `};
 `;
 
 const Slogan = styled.h1`
