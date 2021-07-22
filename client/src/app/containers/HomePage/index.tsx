eb9c5a25cdf148acc72af58bc275596b1efe130e
Michael Pappas <mixalis.pappas23@gmail.com>
2021-07-22 22:52:32 +1000

    fix: fixing a small typo

--- client/src/app/containers/HomePage/index.tsx
+++ client/src/app/containers/HomePage/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import tw from 'twin.macro';
-import { Navbar } from '../../components/navBar';
+import { Navbar } from '../../components/navbar';
 
 
 
