147cf72c2b8af7ba29a1cb8f43e92491099df85c
Michael Pappas <mixalis.pappas23@gmail.com>
2021-07-22 22:10:14 +1000

    style: adjusting the style in logo

--- client/src/app/components/logo/index.tsx
+++ client/src/app/components/logo/index.tsx
@@ -6,31 +6,29 @@ import tw from 'twin.macro';
 import CarLogoImg from '../../../assets/images/car-logo.png';
 
 const LogoContainer = styled.div`
-    ${tw`
-        flex
-        items-center
-    `};
+  ${tw`
+    flex
+    items-center
+  `};
 `;
 
-const LogoText= styled.div`
-    ${tw`
-        text-xl
-        md: text-2xl
-        font-bold
-        text-black
-        m-1
-        
+const LogoText = styled.div`
+  ${tw`
+    text-xl
+    md:text-2xl
+    font-bold
+    text-black
+    m-1
     `};
 `;
 
 const Image = styled.div`
-
+  width: auto;
+  ${tw`h-6 md:h-9`};
+  img {
     width: auto;
-    ${tw`h-6 md:h-9`};
-    img {
-        width: auto;
-        height: 100%;
-    }
+    height: 100%;
+  }
 `;
 
 export function Logo() {
