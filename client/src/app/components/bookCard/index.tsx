779474d9962dc1812b6e987f45eae11509351922
Michael Pappas <mixalis.pappas23@gmail.com>
2021-07-23 01:08:03 +1000

    style: adjusting offeset for mobiles

--- client/src/app/components/bookCard/index.tsx
+++ client/src/app/components/bookCard/index.tsx
@@ -1,19 +1,24 @@
-import { faCalendarAlt, faCaretDown, faCaretUp } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { useState } from "react";
-import styled from "styled-components";
+import {
+  faCalendarAlt,
+  faCaretDown,
+  faCaretUp,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import styled, { css } from "styled-components";
 import tw from "twin.macro";
-import { Marginer } from "../../../marginer";
 import { Button } from "../button";
+
 
 import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
-
+import { SCREENS } from "../responsive";
+import { Marginer } from "../../../marginer";
 
 const CardContainer = styled.div`
-    min-height: 4.3em;
-    box-shadow: 0 1.3px 12px -3px rgba(0, 0, 0, 0.4);
- ${tw`
+  min-height: 4.3em;
+  box-shadow: 0 1.3px 12px -3px rgba(0, 0, 0, 0.4);
+  ${tw`
     flex
     justify-center
     items-center
@@ -27,7 +32,7 @@ const CardContainer = styled.div`
     md:pb-2
     md:pl-9
     md:pr-9
- `};
+  `};
 `;
 
 const ItemContainer = styled.div`
@@ -83,63 +88,70 @@ const DateCalendar = styled(Calendar)`
   user-select: none;
   top: 2em;
   left: 0;
-  `;
+  ${({ offset }: any) =>
+    offset &&
+    css`
+      left: -6em;
+    `};
+  @media (min-width: ${SCREENS.md}) {
+    top: 3.5em;
+    left: -2em;
+  }
+` as any;
 
 export function BookCard() {
-    const [startDate, setStartDate] = useState<Date>(new Date());
-    const [isStartCalendarOpen, setStartCalendarOpen] = useState(false);
-    const [returnDate, setReturnDate] = useState<Date>(new Date());
-    const [isReturnCalendarOpen, setReturnCalendarOpen] = useState(false);
+  const [startDate, setStartDate] = useState<Date>(new Date());
+  const [isStartCalendarOpen, setStartCalendarOpen] = useState(false);
+  const [returnDate, setReturnDate] = useState<Date>(new Date());
+  const [isReturnCalendarOpen, setReturnCalendarOpen] = useState(false);
 
-    const toggleStartDateCalendar = () => {
-        setStartCalendarOpen(!isStartCalendarOpen);
-        if (isReturnCalendarOpen) setReturnCalendarOpen(false);
-    };
+  const toggleStartDateCalendar = () => {
+    setStartCalendarOpen(!isStartCalendarOpen);
+    if (isReturnCalendarOpen) setReturnCalendarOpen(false);
+  };
 
-    const toggleReturnDateCalendar = () => {
-        setReturnCalendarOpen(!isReturnCalendarOpen);
-        if (isStartCalendarOpen) setStartCalendarOpen(false);
-        
-      };
-    
+  const toggleReturnDateCalendar = () => {
+    setReturnCalendarOpen(!isReturnCalendarOpen);
+    if (isStartCalendarOpen) setStartCalendarOpen(false);
+  };
 
-
-
-      return (
-        <CardContainer>
-          <ItemContainer>
-            <Icon>
-              <FontAwesomeIcon icon={faCalendarAlt} />
-            </Icon>
-            <Name onClick={toggleStartDateCalendar}>Pick Up Date</Name>
-            <SmallIcon>
-              <FontAwesomeIcon
-                icon={isStartCalendarOpen ? faCaretUp : faCaretDown}
-              />
-            </SmallIcon>
-            {isStartCalendarOpen && (
-              <DateCalendar value={startDate} onChange={setStartDate as any} />
-            )}
-          </ItemContainer>
-          <LineSeperator />
-          <ItemContainer>
-            <Icon>
-              <FontAwesomeIcon icon={faCalendarAlt} />
-            </Icon>
-            <Name onClick={toggleReturnDateCalendar}>Return Date</Name>
-            <SmallIcon>
-              <FontAwesomeIcon
-                icon={isReturnCalendarOpen ? faCaretUp : faCaretDown}
-              />
-            </SmallIcon>
-            {isReturnCalendarOpen && (
-              <DateCalendar
-                onChange={setReturnDate as any}
-              />
-            )}
-          </ItemContainer>
-          <Marginer direction="horizontal" margin="2em" />
-          <Button text="Book Your Ride" />
-        </CardContainer>
-      );
-    }
+  return (
+    <CardContainer>
+      <ItemContainer>
+        <Icon>
+          <FontAwesomeIcon icon={faCalendarAlt} />
+        </Icon>
+        <Name onClick={toggleStartDateCalendar}>Pick Up Date</Name>
+        <SmallIcon>
+          <FontAwesomeIcon
+            icon={isStartCalendarOpen ? faCaretUp : faCaretDown}
+          />
+        </SmallIcon>
+        {isStartCalendarOpen && (
+          <DateCalendar value={startDate} onChange={setStartDate as any} />
+        )}
+      </ItemContainer>
+      <LineSeperator />
+      <ItemContainer>
+        <Icon>
+          <FontAwesomeIcon icon={faCalendarAlt} />
+        </Icon>
+        <Name onClick={toggleReturnDateCalendar}>Return Date</Name>
+        <SmallIcon>
+          <FontAwesomeIcon
+            icon={isReturnCalendarOpen ? faCaretUp : faCaretDown}
+          />
+        </SmallIcon>
+        {isReturnCalendarOpen && (
+          <DateCalendar
+            offset
+            value={returnDate}
+            onChange={setReturnDate as any}
+          />
+        )}
+      </ItemContainer>
+      <Marginer direction="horizontal" margin="2em" />
+      <Button text="Book Your Ride" />
+    </CardContainer>
+  );
+}
