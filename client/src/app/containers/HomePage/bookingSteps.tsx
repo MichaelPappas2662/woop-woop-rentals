7c4a61892a7242270de1c75a268053e0c7df9ea2
Michael Pappas <mixalis.pappas23@gmail.com>
2021-07-23 01:11:34 +1000

    feat: starting on booking steps

---
+++ client/src/app/containers/HomePage/bookingSteps.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+import tw from "twin.macro";
+
+const Container = styled.div`
+  ${tw``};
+  `;
+
+export function BookingSteps() {
+    return (
+
+    );
+ }
