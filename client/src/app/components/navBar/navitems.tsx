2f70cc30c08e969727b561139c50664907e227b3
Michael Pappas <mixalis.pappas23@gmail.com>
2021-07-22 22:15:15 +1000

    feat: adding the burger-menu in navbar

--- client/src/app/components/navBar/navitems.tsx
+++ client/src/app/components/navBar/navitems.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 import tw from 'twin.macro';
+import { Slide as Menu} from './react-burger-menu';
 
 
 const ListContainer = styled.ul`
