30636de80edb0e78596c835465449825a779ab42
Michael Pappas <mixalis.pappas23@gmail.com>
2021-07-22 22:02:46 +1000

    style: fixing the width of the navbar

--- client/src/app/components/navBar/index.tsx
+++ client/src/app/components/navBar/index.tsx
@@ -7,28 +7,28 @@ import { NavItems } from './navitems';
 
 
 const NavbarContainer = styled.div`
-    min-height: 68px;
-    ${tw`
-        w-full
-        max-w-2xl
-        flex-row
-        items-center
-        lg:pl-12
-        lg:pr-12
-        justify-between
-    `}
-
+  min-height: 68px;
+  ${tw`
+    w-full
+    max-w-screen-2xl
+    flex
+    flex-row
+    items-center
+    lg:pl-12
+    lg:pr-12
+    justify-between
+  `};
 `;
 
-const LogoContainer = styled.div`
-`;
+const LogoContainer = styled.div``;
 
-export function NavBar() {
-    return <NavbarContainer>
-        <LogoContainer>
-            <Logo/>
-        </LogoContainer>
-        <NavItems />
+export function Navbar() {
+  return (
+    <NavbarContainer>
+      <LogoContainer>
+        <Logo />
+      </LogoContainer>
+      <NavItems />
     </NavbarContainer>
-    
+  );
 }
