e846281c0a840528a6a4ec44ee49ffa76d8e08f0
Michael Pappas <mixalis.pappas23@gmail.com>
2021-07-29 18:43:48 +1000

    style: styling the booking steps

--- client/src/app/containers/HomePage/bookingSteps.tsx
+++ client/src/app/containers/HomePage/bookingSteps.tsx
@@ -3,7 +3,14 @@ import styled from "styled-components";
 import tw from "twin.macro";
 
 const Container = styled.div`
-  ${tw``};
+  ${tw`
+    w-full
+    flex
+    flex-col
+    items-center
+    pt-3
+    pb-3
+    `};
   `;
 
 export function BookingSteps() {
