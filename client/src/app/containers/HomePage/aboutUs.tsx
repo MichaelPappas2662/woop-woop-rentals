9ab64f586b98bddd454bec3b3286a0a1fd5cb9f0
Michael Pappas <mixalis.pappas23@gmail.com>
2021-07-29 19:38:32 +1000

    style: creating info and car containers

--- client/src/app/containers/HomePage/aboutUs.tsx
+++ client/src/app/containers/HomePage/aboutUs.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import tw from "twin.macro";
+import { SCREENS } from "../../components/responsive";
 
 const AboutUsContainer = styled.div`
     ${tw`
@@ -19,9 +20,40 @@ const AboutUsContainer = styled.div`
     `};
 `;
 
-const CarContainer = styled.div``;
+const CarContainer = styled.div`
+    width: auto;
+    height: 15em;
+    margin-left: -30px;
 
-const InfoContainer = styled.div``;
+    img{
+        width: auto;
+        height: 100%;
+    }
+
+
+    @media (min-width: ${SCREENS.md}) {
+        height: 28em;
+    }
+
+    @media (min-width: ${SCREENS.lg}) {
+        height: 30em;
+    }
+
+    @media (min-width: ${SCREENS["2xl"]}) {
+        height: 35em;
+        margin-left: 0;
+    }
+`;
+
+const InfoContainer = styled.div`
+    ${tw`
+        w-1/2
+        flex
+        flex-col
+        md:ml-6
+        2xl:ml-16
+    `};
+`;
 
 const Title = styled.h1`
   ${tw``};
