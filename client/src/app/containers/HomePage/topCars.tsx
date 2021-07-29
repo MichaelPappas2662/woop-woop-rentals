6b1f24c46c3ee4f073cb143edee373f12a5eb43c
Michael Pappas <mixalis.pappas23@gmail.com>
2021-07-29 19:58:27 +1000

    style: creating top car container

--- client/src/app/containers/HomePage/topCars.tsx
+++ client/src/app/containers/HomePage/topCars.tsx
@@ -2,7 +2,21 @@ import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import tw from "twin.macro";
 
-const TopCarsContainer = styled.div``;
+const TopCarsContainer = styled.div`
+${tw`
+    max-w-screen-lg
+    w-full
+    flex
+    flex-col
+    items-center
+    justify-center
+    pr-4
+    pl-4
+    md:pl-0
+    md:pr-0
+    mb-10
+  `};
+`;
 
 const Title = styled.h2``;
 
