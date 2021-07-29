6fd40c3c2d3f34b3c26192e46ce2781dd6323804
Michael Pappas <mixalis.pappas23@gmail.com>
2021-07-29 19:30:56 +1000

    feat: starting on about us page

---
+++ client/src/app/containers/HomePage/aboutUs.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import styled from "styled-components";
+import tw from "twin.macro";
+
+const AboutUsContainer = styled.div`
+    ${tw``};
+`;
+
+const CarContainer = styled.div``;
+
+const InfoContainer = styled.div``;
+
+const Title = styled.h1`
+  ${tw``};
+`;
+
+const InfoText = styled.p`
+  ${tw``};
+`;
+
+export function AboutUs() {
+    return ();
