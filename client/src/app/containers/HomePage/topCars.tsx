d0299f463264fa269aea1dfe3e2667de71008b57
Michael Pappas <mixalis.pappas23@gmail.com>
2021-07-29 19:56:25 +1000

    feat: starting on top cars

---
+++ client/src/app/containers/HomePage/topCars.tsx
@@ -0,0 +1,18 @@
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import tw from "twin.macro";
+
+const TopCarsContainer = styled.div``;
+
+const Title = styled.h2``;
+
+
+const CarsContainer = styled.div``;
+
+
+const EmptyCars = styled.div``;
+
+const LoadingContainer = styled.div``;
+
+
+export function TopCars() {};
