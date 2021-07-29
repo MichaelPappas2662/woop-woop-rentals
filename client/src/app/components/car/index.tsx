d1074f61b3b72da74fad5cdb14bfd7e2fbbd5fc1
Michael Pappas <mixalis.pappas23@gmail.com>
2021-07-29 20:40:05 +1000

    fix: importing font awesome

--- client/src/app/components/car/index.tsx
+++ client/src/app/components/car/index.tsx
@@ -1,4 +1,5 @@
 import { faEllipsisH, faFillDrip, faTachometerAlt } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import styled from "styled-components";
 import tw from "twin.macro";
