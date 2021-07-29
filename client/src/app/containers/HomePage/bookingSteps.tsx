ef35c7886c285196b4ae919d5bdcea21f7080861
Michael Pappas <mixalis.pappas23@gmail.com>
2021-07-29 19:03:49 +1000

    style: creating the components style

--- client/src/app/containers/HomePage/bookingSteps.tsx
+++ client/src/app/containers/HomePage/bookingSteps.tsx
@@ -10,8 +10,64 @@ const Container = styled.div`
     items-center
     pt-3
     pb-3
+    lg:pt-6
+    lg:pb-6
     `};
   `;
+
+const Title = styled.h2`
+  ${tw`
+    text-xl
+    lg:text-4xl
+    text-black
+    font-extrabold
+  `};
+`;
+
+const StepsContainer = styled.div`
+  ${tw`
+    flex
+    justify-evenly
+    flex-wrap
+    mt-7
+    lg:mt-16
+  `};
+`;
+
+
+const StepContainer = styled.div`
+  ${tw`
+    flex
+    flex-col
+    w-16
+    md:w-96
+    items-center
+    transition-colors
+    hover:text-red-500
+  `};
+`;
+
+const Step = styled.div`
+  ${tw`
+    flex
+    rounded-lg
+    items-center
+    justify-center
+    p-6
+  `};
+`;
+
+const StepTitle = styled.h4`
+  ${tw`
+    text-black
+    text-lg
+    font-semibold
+    mt-4
+  `};
+`;
+
+const StepDescription = styled.p
+
 
 export function BookingSteps() {
     return (
