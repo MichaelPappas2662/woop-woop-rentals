d8b1d5dfbb0c616f003b6be0a6442d5d3ef4d93d
Michael Pappas <mixalis.pappas23@gmail.com>
2021-07-30 13:47:33 +1000

    style: starting on the footer

---
+++ client/src/app/components/footer/index.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import styled from "styled-components";
+import tw from "twin.macro";
+
+
+const FooterContainer = styled.div`
+    min-height: 20em;
+    ${tw`
+        flex
+        flex-wrap
+        min-w-full
+        bg-blue-900
+        pt-7
+        pb-1
+
+    `};
+`;
+
+export function Footer() {
+    
+}
