e0a83003c94e5819967d3ce5730bf2dbc6d0b54d
Michael Pappas <mixalis.pappas23@gmail.com>
2021-08-02 14:38:50 +1000

    feat: adding services for Graphql

--- server/src/cars/cars.module.ts
+++ server/src/cars/cars.module.ts
@@ -1,6 +1,9 @@
 import { Module } from '@nestjs/common';
+import { CarsService } from './cars.service';
 
 @Module({
   imports: [],
+  providers: [CarsService],
+  exports: [CarsService],
 })
 export class CarsModule {}
