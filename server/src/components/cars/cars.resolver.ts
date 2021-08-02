04e8ba2080c2b676476dc1d15abc156c3f9f3761
Michael Pappas <mixalis.pappas23@gmail.com>
2021-08-02 15:36:03 +1000

    feat: creating a call to all cars

--- server/src/components/cars/cars.resolver.ts
+++ server/src/components/cars/cars.resolver.ts
@@ -1,12 +1,13 @@
 import { Resolver, Query } from '@nestjs/graphql';
 import { CarsService } from './cars.service';
+import { Car } from './entities/car';
 
 @Resolver()
 export class CarsResolver {
   constructor(private carsService: CarsService) {}
 
-  @Query((returns) => String)
-  public async cars() {
-    return 'Hello and welcome to Woop Woop Rentals shop!';
+  @Query((returns) => [Car])
+  public async cars(): Promise<Car[]> {
+    return await this.carsService.getAllCars();
   }
 }
