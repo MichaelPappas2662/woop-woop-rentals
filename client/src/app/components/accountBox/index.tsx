105dca35690a16c2a727d088776d76621d6523af
Michael Pappas <mixalis.pappas23@gmail.com>
2021-08-02 20:35:08 +1000

    feat: starting on login page

---
+++ client/src/app/components/accountBox/index.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import styled from "styled-components";
+
+const BoxContainer = styled.div`
+  width: 280px;
+  min-height: 550px;
+  display: flex;
+  flex-direction: column;
+  border-radius: 19px;
+  background-color: #fff;
+  box-shadow: 0 0 2px rgba(15, 15, 15, 0.28);
+  position: relative;
+  overflow: hidden;
+`;
+
+export function AccountBox(props) {
+    
+}
