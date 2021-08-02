66d2f288cd105d3395d36e93515a5e73b994df7a
Michael Pappas <mixalis.pappas23@gmail.com>
2021-08-02 21:50:47 +1000

    fix: trying to fix react - dom

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Route, Switch as Switching } from "react-router";
 import styled from 'styled-components';
 import tw from 'twin.macro';
 import './App.css';
 import { AccountBox } from './app/components/accountBox';
 import { HomePage } from './app/containers/HomePage';
+
 
 const AppContainer = styled.div`
   ${tw`
@@ -14,11 +16,18 @@ const AppContainer = styled.div`
     `};
 `;
 
+
 function App() {
-  return <AppContainer>
-    <AccountBox />
-    <HomePage></HomePage>
-    </AppContainer>
+    return (
+    
+      <AppContainer>
+     
+      <AccountBox />
+      <HomePage />
+    
+      </AppContainer>
+  )
+  
 }
 
 export default App;
