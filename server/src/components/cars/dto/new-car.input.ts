826e4b04a1117c7df6188b5cb281419a1cc7928d
Michael Pappas <mixalis.pappas23@gmail.com>
2021-08-02 16:09:20 +1000

    feat: adding validation on prices

--- server/src/components/cars/dto/new-car.input.ts
+++ server/src/components/cars/dto/new-car.input.ts
@@ -1,15 +1,20 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Field, InputType, Int } from '@nestjs/graphql';
-import { type } from 'os';
+import { Max, Min } from 'class-validator';
 
 @InputType()
 export class NewCarInput {
   @Field()
   name: string;
 
-  @Field((type) => Int)
+  @Field((_type) => Int)
+  @Max(20000)
+  @Min(1500)
   monthlyPrice: number;
 
-  @Field((type) => Int)
+  @Field((_type) => Int)
+  @Max(1000)
+  @Min(10)
   dailyPrice: number;
 
   @Field()
