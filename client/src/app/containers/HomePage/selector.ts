fd8a394944aac2b7cc9af3f570a1b45c1f0781fc
Michael Pappas <mixalis.pappas23@gmail.com>
2021-08-02 19:00:11 +1000

    fix: passing the homepage interface into selector

--- client/src/app/containers/HomePage/selector.ts
+++ client/src/app/containers/HomePage/selector.ts
@@ -1,5 +1,6 @@
 import { createSelector } from "reselect";
+import { IRootAppState } from "../../../typings";
 
-const selectHomePage = (state: any) => state.homePage;
+const selectHomePage = (state: IRootAppState) => state.homePage;
 
 export const makeSelectTopCars = createSelector(selectHomePage, (homePage) => homePage.topCars)
