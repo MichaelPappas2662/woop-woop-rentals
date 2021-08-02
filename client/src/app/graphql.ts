91732b143be93e19370fd5e38159bc3ccbebf4a4
Michael Pappas <mixalis.pappas23@gmail.com>
2021-08-02 17:07:52 +1000

    fix: fixing an env error

--- client/src/app/graphql.ts
+++ client/src/app/graphql.ts
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const apolloClient = new ApolloClient({
-    uri: process.env.REACT_APP_API_URL as any,
-    cache: new InMemoryCache(),
-})
+  uri: `${process.env.REACT_APP_API_URL}/graphql` as any,
+  cache: new InMemoryCache(),
+});
