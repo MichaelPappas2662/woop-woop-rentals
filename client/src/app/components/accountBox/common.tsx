a0d2471c1d93267bc1e83ced5dde5a627627a2b3
Michael Pappas <mixalis.pappas23@gmail.com>
2021-08-03 09:30:55 +1000

    style: adding a muted link for common use

--- client/src/app/components/accountBox/common.tsx
+++ client/src/app/components/accountBox/common.tsx
@@ -15,3 +15,10 @@ export const FormContainer = styled.form`
   flex-direction: column;
   box-shadow: 0px 0px 2.5px rgba(15, 15, 15, 0.19);
 `;
+
+export const MutedLink = styled.a`
+  font-size: 11px;
+  color: rgba(200, 200, 200, 0.8);
+  font-weight: 500;
+  text-decoration: none;
+`;
