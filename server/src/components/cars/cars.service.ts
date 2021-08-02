046fcb1e58d5319235a3f17bdd029f3f578fd1dc
Michael Pappas <mixalis.pappas23@gmail.com>
2021-08-02 18:17:51 +1000

    chore: disable eslint error

--- server/src/components/cars/cars.service.ts
+++ server/src/components/cars/cars.service.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
