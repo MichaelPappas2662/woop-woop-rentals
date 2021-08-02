afecc7a1597ad7e11669f8717164b9c9d220b915
Michael Pappas <mixalis.pappas23@gmail.com>
2021-08-02 14:21:31 +1000

    fix: fixing the sql connection

--- server/src/database/database.module.ts
+++ server/src/database/database.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
-import { Connection } from 'typeorm';
+import { Connection, getConnectionOptions } from 'typeorm';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [TypeOrmModule.forRoot()],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      useFactory: async () =>
+        Object.assign(
+          await getConnectionOptions(
+            process.env.NODE_ENV === 'production' ? 'prod' : 'dev',
+          ),
+        ),
+    }),
+  ],
   exports: [TypeOrmModule],
 })
 export class DatabaseModule {
