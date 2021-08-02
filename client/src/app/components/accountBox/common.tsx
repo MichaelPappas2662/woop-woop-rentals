9bffbab67455b58ef4e26c6c82e26469e878ef1b
Michael Pappas <mixalis.pappas23@gmail.com>
2021-08-03 09:29:30 +1000

    style: adding common sign/login components

---
+++ client/src/app/components/accountBox/common.tsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+
+export const BoxContainer = styled.div`
+width: 100%;
+display: flex;
+flex-direction: column;
+align-items: center;
+margin-top: 10px;
+`;
+
+export const FormContainer = styled.form`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  box-shadow: 0px 0px 2.5px rgba(15, 15, 15, 0.19);
+`;
