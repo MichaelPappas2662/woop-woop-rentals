e8b0b0925aa0182a1f60166dcf4b7776d7887cc5
Michael Pappas <mixalis.pappas23@gmail.com>
2021-08-03 09:38:35 +1000

    style: adding a style inpute

--- client/src/app/components/accountBox/common.tsx
+++ client/src/app/components/accountBox/common.tsx
@@ -30,3 +30,24 @@ export const BoldLink = styled.a`
   text-decoration: none;
   margin: 0 4px;
 `;
+
+export const Input = styled.input`
+  width: 100%;
+  height: 42px;
+  outline: none;
+  border: 1px solid rgba(200, 200, 200, 0.3);
+  padding: 0px 10px;
+  border-bottom: 1.4px solid transparent;
+  transition: all 200ms ease-in-out;
+  font-size: 12px;
+  &::placeholder {
+    color: rgba(200, 200, 200, 1);
+  }
+  &:not(:last-of-type) {
+    border-bottom: 1.5px solid rgba(200, 200, 200, 0.4);
+  }
+  &:focus {
+    outline: none;
+    border-bottom: 2px solid rgb(161, 3, 3)s;
+  }
+`;
