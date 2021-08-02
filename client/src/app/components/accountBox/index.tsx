da783ebb643845aa7de6b3a468bb4dee80218bda
Michael Pappas <mixalis.pappas23@gmail.com>
2021-08-02 20:40:11 +1000

    style: adding the background of login

--- client/src/app/components/accountBox/index.tsx
+++ client/src/app/components/accountBox/index.tsx
@@ -13,6 +13,24 @@ const BoxContainer = styled.div`
   overflow: hidden;
 `;
 
+const BackDrop = styled.div`
+  width: 160%;
+  height: 550px;
+  position: absolute;
+  display: flex;
+  flex-direction: column;
+  border-radius: 50%;
+  transform: rotate(60deg);
+  top: -290px;
+  left: -70px;
+  background: #c50d0d;
+  background: linear-gradient(
+    58deg,
+    #c50d0d 20%,
+    #c50d0d 100%
+  
+`;
+
 export function AccountBox(props) {
     
 }
