23ee562273b3c06f6ade6c2eddc847640570a46b
Michael Pappas <mixalis.pappas23@gmail.com>
2021-08-03 09:29:10 +1000

    fix: fixing the router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
-import { Route, Switch as Switching } from "react-router";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import styled from 'styled-components';
 import tw from 'twin.macro';
 import './App.css';
@@ -21,10 +26,16 @@ function App() {
     return (
     
       <AppContainer>
-     
+    <Router>
+     <Switch>
+       <Route path="/login">
       <AccountBox />
+      </Route>
+      <Route path="/homepage">
       <HomePage />
-    
+      </Route>
+      </Switch>
+    </Router>
       </AppContainer>
   )
   
