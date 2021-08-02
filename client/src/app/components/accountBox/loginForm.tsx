f409e0ed19359e5177028ed2297e8f2db99167a1
Michael Pappas <mixalis.pappas23@gmail.com>
2021-08-03 09:52:35 +1000

    feat: starting on the login form

--- client/src/app/components/accountBox/loginForm.tsx
+++ client/src/app/components/accountBox/loginForm.tsx
@@ -1,3 +1,14 @@
 import React from "react";
+import { BoxContainer, FormContainer, Input } from "./common";
 
-export {}
+export function LoginForm() {
+
+    return (
+        <BoxContainer>
+            <FormContainer>
+                <Input type="email" placeholder="Email"/>
+                <Input type="password" placeholder="Password"/>
+            </FormContainer>
+        </BoxContainer>
+    )
+}
