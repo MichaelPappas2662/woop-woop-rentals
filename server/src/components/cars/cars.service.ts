56f4d299bc1a8c8fbdb526fb5a2cfc23f49ba810
Michael Pappas <mixalis.pappas23@gmail.com>
2021-08-02 15:32:51 +1000

    feat:  injecting db

--- server/src/components/cars/cars.service.ts
+++ server/src/components/cars/cars.service.ts
@@ -1,4 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Car } from './entities/car';
 
 @Injectable()
-export class CarsService {}
+export class CarsService {
+  constructor(@InjectRepository(Car) private carRepository: Repository<Car>) {}
+
+  public async getCars(): Promise<Car[]> {
+    return await this.carRepository.find({}).catch((err) => {
+      throw new InternalServerErrorException();
+    });
+  }
+}
