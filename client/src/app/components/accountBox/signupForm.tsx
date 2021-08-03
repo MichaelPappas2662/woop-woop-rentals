a17b6db849aee0f1a105baf98cb6366062c5469c
Michael Pappas <mixalis.pappas23@gmail.com>
2021-08-03 10:31:35 +1000

    style: starting on the signup

--- client/src/app/components/accountBox/signupForm.tsx
+++ client/src/app/components/accountBox/signupForm.tsx
@@ -1,3 +1,36 @@
 import React from "react";
+import { Marginer } from "../../../marginer";
+import {
+  BoldLink,
+  BoxContainer,
+  FormContainer,
+  Input,
+  MutedLink,
+  SubmitButton,
+} from "./common";
 
-export function SignupForm() {}
+
+
+export function SignupForm() {
+  ;
+
+  return (
+    <BoxContainer>
+      <FormContainer>
+        <Input type="text" placeholder="Full Name" />
+        <Input type="email" placeholder="Email" />
+        <Input type="password" placeholder="Password" />
+        <Input type="password" placeholder="Confirm Password" />
+      </FormContainer>
+      <Marginer direction="vertical" margin={10} />
+      <SubmitButton type="submit">Signup</SubmitButton>
+      <Marginer direction="vertical" margin="1em" />
+      <MutedLink href="#">
+        Already have an account?
+        <BoldLink href="#" >
+          SignIn
+        </BoldLink>
+      </MutedLink>
+    </BoxContainer>
+  );
+}
