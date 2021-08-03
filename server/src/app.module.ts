6a7345fdeb4c08e2fae1db80c78e4fc21f66ba30
Michael Pappas <mixalis.pappas23@gmail.com>
2021-08-03 17:08:23 +1000

    feat: adding postgress module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -5,10 +5,19 @@ import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ComponentsModule } from './components/components.module';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
     DatabaseModule,
     GraphQLModule.forRoot({
       playground: true,
@@ -16,6 +25,8 @@ import { ComponentsModule } from './components/components.module';
       autoSchemaFile: true,
     }),
     ComponentsModule,
+    UserModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
