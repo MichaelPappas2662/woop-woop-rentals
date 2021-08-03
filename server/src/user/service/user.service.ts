96bcc66bce6d3644eaeaba1a65ba0749917e1fb6
Michael Pappas <mixalis.pappas23@gmail.com>
2021-08-03 17:10:01 +1000

    feat: importing module and creating single user

--- server/src/user/service/user.service.ts
+++ server/src/user/service/user.service.ts
@@ -2,6 +2,7 @@ import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { from, Observable } from 'rxjs';
 import { map, switchMap } from 'rxjs/operators';
+import { AuthService } from 'src/auth/services/auth/auth.service';
 import { Repository } from 'typeorm';
 import { CreateUserDto } from '../models/dto/CreateUser.dto';
 import { LoginUserDto } from '../models/dto/LoginUser.dto';
@@ -13,7 +14,61 @@ export class UserService {
   constructor(
     @InjectRepository(UserEntity)
     private userRepository: Repository<UserEntity>,
+    private authService: AuthService,
   ) {}
+
+  create(createdUserDto: CreateUserDto): Observable<UserI> {
+    return this.mailExists(createdUserDto.email).pipe(
+      switchMap((exists: boolean) => {
+        if (!exists) {
+          return this.authService.hashPassword(createdUserDto.password).pipe(
+            switchMap((passwordHash: string) => {
+              // Overwrite the user password with the hash, to store it in the db
+              createdUserDto.password = passwordHash;
+              return from(this.userRepository.save(createdUserDto)).pipe(
+                map((savedUser: UserI) => {
+                  const { password, ...user } = savedUser;
+                  return user;
+                }),
+              );
+            }),
+          );
+        } else {
+          throw new HttpException('Email already in use', HttpStatus.CONFLICT);
+        }
+      }),
+    );
+  }
+
+  login(loginUserDto: LoginUserDto): Observable<string> {
+    return this.findUserByEmail(loginUserDto.email).pipe(
+      switchMap((user: UserI) => {
+        if (user) {
+          return this.validatePassword(
+            loginUserDto.password,
+            user.password,
+          ).pipe(
+            switchMap((passwordsMatches: boolean) => {
+              if (passwordsMatches) {
+                return this.findOne(user.id).pipe(
+                  switchMap((user: UserI) =>
+                    this.authService.generateJwt(user),
+                  ),
+                );
+              } else {
+                throw new HttpException(
+                  'Login was not Successful',
+                  HttpStatus.UNAUTHORIZED,
+                );
+              }
+            }),
+          );
+        } else {
+          throw new HttpException('User not found', HttpStatus.NOT_FOUND);
+        }
+      }),
+    );
+  }
 
   findAll(): Observable<UserI[]> {
     return from(this.userRepository.find());
@@ -29,6 +84,25 @@ export class UserService {
         { email },
         { select: ['id', 'email', 'name', 'password'] },
       ),
+    );
+  }
+
+  private validatePassword(
+    password: string,
+    storedPasswordHash: string,
+  ): Observable<boolean> {
+    return this.authService.comparePasswords(password, storedPasswordHash);
+  }
+
+  private mailExists(email: string): Observable<boolean> {
+    return from(this.userRepository.findOne({ email })).pipe(
+      map((user: UserI) => {
+        if (user) {
+          return true;
+        } else {
+          return false;
+        }
+      }),
     );
   }
 }
