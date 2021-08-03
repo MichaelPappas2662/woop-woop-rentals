9e55f1135a458e1fe2d1b4d3518e65cd95bb8cfa
Michael Pappas <mixalis.pappas23@gmail.com>
2021-08-03 12:58:28 +1000

    feat: creating a migration

---
+++ server/src/migrations/1627959485570-CreateDataBase.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class CreateDataBase1627959485570 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
